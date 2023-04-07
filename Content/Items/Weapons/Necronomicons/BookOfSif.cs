b1be70034f123b4a878c77668a2905b905714a56
Tea <66430824+Minecrafty33@users.noreply.github.com>
2023-04-07 20:32:34 +0200

    Added random projectiles to Book of Sif and fixed unreachable code error

--- Content/Items/Weapons/Necronomicons/BookOfSif.cs
+++ Content/Items/Weapons/Necronomicons/BookOfSif.cs
@@ -179,9 +179,8 @@ namespace WraithMod.Content.Items.Weapons.Necronomicons
                     yellow.noGravity = true;
                 }
                 SoundEngine.PlaySound(SoundID.MaxMana);
-                //int type2 = Main.rand.Next(new int[] { ModContent.ProjectileType<Blinkroot>(), ModContent.ProjectileType<Daybloom>(), ModContent.ProjectileType<Deathweed>(), ModContent.ProjectileType<Fireblossom>(), ModContent.ProjectileType<Moonglow>(), ModContent.ProjectileType<Shiverthorn>(), ModContent.ProjectileType<Waterleaf>()});
-                //Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Vector2.Zero, type2, Projectile.damage, Projectile.knockBack, Projectile.owner);
-                Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<Daybloom>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
+                int type2 = Main.rand.Next(new int[] { ModContent.ProjectileType<Blinkroot>(), ModContent.ProjectileType<Daybloom>(), ModContent.ProjectileType<Deathweed>(), ModContent.ProjectileType<Fireblossom>(), ModContent.ProjectileType<Moonglow>(), ModContent.ProjectileType<Shiverthorn>(), ModContent.ProjectileType<Waterleaf>()});
+                Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Vector2.Zero, type2, Projectile.damage, Projectile.knockBack, Projectile.owner);
                 Timer = 0;
             }
             Dust green2 = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.GreenTorch, 0f, 0f, 150, default(Color), 1.5f);
@@ -198,7 +197,6 @@ namespace WraithMod.Content.Items.Weapons.Necronomicons
             var bloom = color;
             bloom.A = 0;
             Main.EntitySpriteDraw(bloomTex.Value, Projectile.Center - Main.screenPosition, null, bloom, Projectile.rotation, bloomTex.Size() * 0.5f, Projectile.scale * 1.3f, SpriteEffects.None, 0);
-            return true;
             // SpriteEffects helps to flip texture horizontally and vertically
             SpriteEffects spriteEffects = SpriteEffects.None;
             if (Projectile.spriteDirection == -1)
