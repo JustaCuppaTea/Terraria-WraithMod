1caaca57c4ef523b151c6d0c07b44b0728a1797e
Tea <66430824+Minecrafty33@users.noreply.github.com>
2023-04-08 01:34:41 +0200

    Book of sif changes
    Added ritual circle effect around mouse while using the item

---
+++ Content/Items/Weapons/Necronomicons/BookOfEir/BookOfEir.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WraithMod.Content.Items.Weapons.Necronomicons.BookOfEir
+{
+    internal class BookOfEir
+    {
+    }
+}
