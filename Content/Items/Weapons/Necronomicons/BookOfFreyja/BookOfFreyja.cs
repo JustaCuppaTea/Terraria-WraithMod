a80fb3deb2ee57f58a9252b0329104bf1cd56974
Tea <66430824+Minecrafty33@users.noreply.github.com>
2023-04-10 02:11:01 +0200

    Book of freyja changes, added attack

--- Content/Items/Weapons/Necronomicons/BookOfFreyja/BookOfFreyja.cs
+++ Content/Items/Weapons/Necronomicons/BookOfFreyja/BookOfFreyja.cs
@@ -16,6 +16,7 @@ using WraithMod.Content.Tiles.Furniture;
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
 using WraithMod.Content.Items.Weapons.Necronomicons.BookOfSif;
+using Terraria.Audio;
 
 namespace WraithMod.Content.Items.Weapons.Necronomicons.BookOfFreyja
 {
@@ -101,10 +102,6 @@ namespace WraithMod.Content.Items.Weapons.Necronomicons.BookOfFreyja
                 .AddTile(ModContent.TileType<RitualTable>())
                 .Register();
         }
-        public override void HoldItem(Player player)
-        {
-            base.HoldItem(player);
-        }
     }
     public class BookOfFreyjaProj : ModProjectile
     {
@@ -174,6 +171,20 @@ namespace WraithMod.Content.Items.Weapons.Necronomicons.BookOfFreyja
             {
                 Projectile.timeLeft += 10;
             }
+            Timer++;
+            if (Timer > BookOfFreyja.TimeUse * 2)
+            {
+                // Our timer has finished, do something here:
+                // SoundEngine.PlaySound, Dust.NewDust, Projectile.NewProjectile, etc. Up to you.
+                for (int d = 0; d < 10; d++)
+                {
+                    Dust yellow = Dust.NewDustDirect(Projectile.Center, Projectile.width, Projectile.height, DustID.YellowTorch, 0f, 0f, 150, default, 1.5f);
+                    yellow.noGravity = true;
+                }
+                SoundEngine.PlaySound(SoundID.MaxMana);
+                Projectile.NewProjectile(Projectile.GetSource_FromThis(), player.position, Vector2.Zero, ModContent.ProjectileType<CharmPulse>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
+                Timer = 0;
+            }
         }
         // Some advanced drawing because the texture image isn't centered or symetrical
         // If you dont want to manually drawing you can use vanilla projectile rendering offsets
@@ -229,6 +240,82 @@ namespace WraithMod.Content.Items.Weapons.Necronomicons.BookOfFreyja
     }
     public class CharmPulse : ModProjectile
     {
+        public float Timer
+        {
+            get => Projectile.ai[0];
+            set => Projectile.ai[0] = value;
+        }
+        public override void SetStaticDefaults()
+        {
+            // Total count animation frames
+            Main.projFrames[Projectile.type] = 9;
+        }
+        public override void SetDefaults()
+        {
+            Projectile.friendly = true;
+            Projectile.DamageType = ModContent.GetInstance<WraithClass>();
+            Projectile.tileCollide = false;
+            Projectile.Size = new Vector2(128, 128);
+            Projectile.penetrate = -1;
+            Projectile.aiStyle = 0;
+        }
+        public override void AI()
+        {
+            Timer++;
+            if (Timer >= 108)
+            {
+                Projectile.Kill();
+            }
+            if (++Projectile.frameCounter >= 12)
+            {
+                Projectile.frameCounter = 0;
+                // Or more compactly Projectile.frame = ++Projectile.frame % Main.projFrames[Projectile.type];
+                if (++Projectile.frame >= Main.projFrames[Projectile.type])
+                    Projectile.frame = 0;
+            }
+        }
 
+        public override bool PreDraw(ref Color lightColor)
+        {
+            // SpriteEffects helps to flip texture horizontally and vertically
+            SpriteEffects spriteEffects = SpriteEffects.None;
+            if (Projectile.spriteDirection == -1)
+                spriteEffects = SpriteEffects.FlipHorizontally;
+
+            // Getting texture of projectile
+            Texture2D texture = (Texture2D)ModContent.Request<Texture2D>(Texture);
+
+            // Calculating frameHeight and current Y pos dependence of frame
+            // If texture without animation frameHeight is always texture.Height and startY is always 0
+            int frameHeight = texture.Height / Main.projFrames[Projectile.type];
+            int startY = frameHeight * Projectile.frame;
+
+            // Get this frame on texture
+            Rectangle sourceRectangle = new Rectangle(0, startY, texture.Width, frameHeight);
+
+            // Alternatively, you can skip defining frameHeight and startY and use this:
+            // Rectangle sourceRectangle = texture.Frame(1, Main.projFrames[Projectile.type], frameY: Projectile.frame);
+
+            Vector2 origin = sourceRectangle.Size() / 2f;
+
+            // If image isn't centered or symmetrical you can specify origin of the sprite
+            // (0,0) for the upper-left corner
+            float offsetX = 20f;
+            origin.X = Projectile.spriteDirection == 1 ? sourceRectangle.Width - offsetX : offsetX;
+
+            // If sprite is vertical
+            // float offsetY = 20f;
+            // origin.Y = (float)(Projectile.spriteDirection == 1 ? sourceRectangle.Height - offsetY : offsetY);
+
+
+            // Applying lighting and draw current frame
+            Color drawColor = Color.White;
+            Main.EntitySpriteDraw(texture,
+                Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY),
+                sourceRectangle, drawColor, Projectile.rotation, origin, Projectile.scale, spriteEffects, 0);
+
+            // It's important to return false, otherwise we also draw the original texture.
+            return false;
+        }
     }
 }
